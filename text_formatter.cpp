53179f611a4a8c1554f6f571547a4ffc5325967e
Andrei Tudor TOPALĂ (101668) <topala.andrei@gmail.com>
2020-12-31 03:47:29 +0200

    parsed textfile

--- text_formatter.cpp
+++ text_formatter.cpp
@@ -2,8 +2,38 @@
 #include <bits/stdc++.h>
 
 #define ROOT 0
+#define MASTER_THREADS 4
+#define HORROR_ID 0
+#define COMEDY_ID 1
+#define FANTASY_ID 2
+#define SCIFI_ID 3
 
 using namespace std;
+
+void parse_paragraph(ifstream &fin) {
+	std::string line;
+
+	do {
+		getline(fin, line);
+	} while (line != "");
+}
+
+void parse(int id) {
+	ifstream fin("input.txt");
+	std::string line;
+
+	while (getline(fin, line)) {
+		if (line == "horror" && id == HORROR_ID) {
+			parse_paragraph(fin);
+		} else if (line == "comedy" && id == COMEDY_ID) {
+			parse_paragraph(fin);
+		} else if (line == "fantasy" && id == FANTASY_ID) {
+			parse_paragraph(fin);
+		} else if (line == "science-fiction" && id == SCIFI_ID) {
+			parse_paragraph(fin);
+		} 
+ 	}
+}
 
 int main(int argc, char *argv[]) {
 	int numtasks, rank, provided;
@@ -12,6 +42,17 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
+    if (rank == ROOT) {
+    	std::thread parsers[MASTER_THREADS];
+
+    	for (int i = 0; i < MASTER_THREADS; ++i) {
+    		parsers[i] = std::thread(parse, i);
+    	}
+
+    	for (int i = 0; i < MASTER_THREADS; ++i) {
+    		parsers[i].join();
+    	}
+    }
 
 	MPI_Finalize();
 	return 0;
