0a7f741b609d991c9f516224fc121b9027c5f9c8
Andrei Tudor TOPALĂ (101668) <topala.andrei@gmail.com>
2021-01-04 02:37:08 +0200

    implemented producer-consumer

---
+++ producers_consumers.h
@@ -0,0 +1,134 @@
+#pragma once
+
+#include "mpi.h"
+#include <bits/stdc++.h>
+#include <atomic>
+
+#define ROOT 0
+#define MAX_PARAGRAPH 10000
+#define END_OF_FILE "==EOF=="
+
+using namespace std;
+
+class Producer_Consumer {
+ private:
+ 	string buffer;
+ 	std::mutex mtx;
+	std::condition_variable produce, consume;
+	unsigned int line_nr;
+	int consumers_nr;
+	std::atomic<bool> end;
+	std::atomic<unsigned int> current_ticket;
+	pthread_barrier_t barrier;
+
+	int count_lines(string buffer) {
+		int ans = 0;
+
+		for (unsigned int i = 0; i < buffer.size(); ++i) {
+			if (buffer[i] == '\n') {
+				++ans;
+			}
+		}
+		return ans;
+	}
+
+	unsigned int get_ticket() {
+		return current_ticket++;
+	}
+
+	bool consonant(char letter) {
+		return letter != 'A' && letter != 'E' && letter != 'I' && letter != 'O' && letter != 'U' &&
+			letter != 'a' && letter != 'e' && letter != 'i' && letter != 'o' && letter != 'u' && 
+			letter != '\n' && letter != '.' && letter != ' ';
+	}
+
+ public:
+ 	Producer_Consumer() {
+ 		end = false;
+ 		current_ticket = 0;
+ 		pthread_barrier_init(&barrier, NULL, std::thread::hardware_concurrency());
+ 	}
+
+ 	void parallelise() {
+ 		int num_threads = std::thread::hardware_concurrency() - 1;
+    	std::thread consumers[num_threads];
+    	for (int i = 0; i < num_threads; ++i) {
+    		consumers[i] = std::thread(&Producer_Consumer::consumer, this);
+    	}
+    	std::thread receiver = std::thread(&Producer_Consumer::receiver, this);
+
+		for (int i = 0; i < num_threads; ++i) {
+			consumers[i].join();
+		}
+		receiver.join();
+
+ 	}
+
+ 	void receiver() {
+ 		std::unique_lock<std::mutex> lck(mtx);
+ 		char buff[MAX_PARAGRAPH] = {0};
+		do {
+			MPI_Status status;
+			int length = 0;
+
+			MPI_Probe(ROOT, 0, MPI_COMM_WORLD, &status);
+			MPI_Get_count(&status, MPI_CHAR, &length);
+
+			MPI_Recv(&buff, length, MPI_CHAR, ROOT, 0, MPI_COMM_WORLD, &status);
+			buffer = buff;
+			line_nr = count_lines(buffer) - 1;
+			consumers_nr = std::min(1 + line_nr / 20, std::thread::hardware_concurrency() - 1);
+
+			if (buffer != END_OF_FILE) {
+				for (int i = 0; i < consumers_nr; ++i) {
+					consume.notify_one();
+				}
+
+				produce.wait(lck);
+			}
+		} while(strcmp(buff, END_OF_FILE));
+		end = true;
+		consume.notify_all();
+	}
+
+ 	void consumer() {
+ 		std::unique_lock<std::mutex> lck(mtx);
+		while(!end.load()) {
+			consume.wait(lck);
+			if (end.load() == true) {
+				break;
+			}
+		 	int ticket = get_ticket();
+		 	int remaining_lines = 0;
+		 	while(ticket <= line_nr / 20) {
+		 		remaining_lines = 20 * ticket + 1;
+		 		int i = 0;
+		 		while (i < buffer.size() && remaining_lines > 0) {
+		 			if (buffer[i] == '\n') {
+		 				--remaining_lines;
+		 			}
+		 			++i;
+		 		}
+		 		remaining_lines = 20;
+		 		while (i < buffer.size() && remaining_lines > 0) {
+		 			if (buffer[i] == '\n') {
+		 				--remaining_lines;
+		 			}
+		 			if (consonant(buffer[i])) {
+		 				std::string lower (1, std::tolower(buffer[i]));
+		 				buffer.insert(i + 1, lower);
+		 				++i;
+		 			}
+		 			++i;
+		 		}
+
+		 		ticket = get_ticket();
+		 	}
+		 	cout << buffer << endl;
+		 	if (remaining_lines > 0) {
+		 		produce.notify_one();
+		 		current_ticket = 0;
+		 	}
+		}
+	}
+};
--- text_formatter.cpp
+++ text_formatter.cpp
@@ -1,38 +1,55 @@
 #include "mpi.h"
 #include <bits/stdc++.h>
+#include <condition_variable>
+#include "producers_consumers.h"
 
 #define ROOT 0
 #define MASTER_THREADS 4
-#define HORROR_ID 0
-#define COMEDY_ID 1
-#define FANTASY_ID 2
-#define SCIFI_ID 3
+#define HORROR_ID 1
+#define COMEDY_ID 2
+#define FANTASY_ID 3
+#define SCIFI_ID 4
+#define MAX_PARAGRAPH 10000
+#define END_OF_FILE "==EOF=="
 
 using namespace std;
 
-void parse_paragraph(ifstream &fin) {
+string parse_paragraph(ifstream &fin) {
+	std::string ans = "";
 	std::string line;
 
 	do {
+		ans += '\n';
 		getline(fin, line);
+		ans += line;
 	} while (line != "");
+
+	return ans;
 }
 
 void parse(int id) {
 	ifstream fin("input.txt");
 	std::string line;
+	std::string paragraph;
 
 	while (getline(fin, line)) {
 		if (line == "horror" && id == HORROR_ID) {
-			parse_paragraph(fin);
+			paragraph = "horror" + parse_paragraph(fin);
+			MPI_Send(&paragraph[0], paragraph.size() + 1, MPI_CHAR, id, 0, MPI_COMM_WORLD); 
 		} else if (line == "comedy" && id == COMEDY_ID) {
-			parse_paragraph(fin);
+			paragraph = "comedy" + parse_paragraph(fin);
+			MPI_Send(&paragraph[0], paragraph.size() + 1, MPI_CHAR, id, 0, MPI_COMM_WORLD); 
 		} else if (line == "fantasy" && id == FANTASY_ID) {
-			parse_paragraph(fin);
+			paragraph = "fantasy" + parse_paragraph(fin);
+			MPI_Send(&paragraph[0], paragraph.size() + 1, MPI_CHAR, id, 0, MPI_COMM_WORLD); 
 		} else if (line == "science-fiction" && id == SCIFI_ID) {
-			parse_paragraph(fin);
+			paragraph = "science-fiction" + parse_paragraph(fin);
+			MPI_Send(&paragraph[0], paragraph.size() + 1, MPI_CHAR, id, 0, MPI_COMM_WORLD); 
 		} 
  	}
+
+ 	paragraph = END_OF_FILE;
+ 	MPI_Send(&paragraph[0], paragraph.size() + 1, MPI_CHAR, id, 0, MPI_COMM_WORLD); 
 }
 
 int main(int argc, char *argv[]) {
@@ -46,13 +63,23 @@ int main(int argc, char *argv[]) {
     	std::thread parsers[MASTER_THREADS];
 
     	for (int i = 0; i < MASTER_THREADS; ++i) {
-    		parsers[i] = std::thread(parse, i);
+    		parsers[i] = std::thread(parse, i + 1);
     	}
 
     	for (int i = 0; i < MASTER_THREADS; ++i) {
     		parsers[i].join();
     	}
-    }
+    } else if (rank == HORROR_ID) {
+    	Producer_Consumer horror;
+    	horror.parallelise();
+    	
+    } else if (rank == COMEDY_ID) {
+    	// TODO
+    } else if (rank == FANTASY_ID) {
+    	// TODO
+    } else if (rank == SCIFI_ID) {
+    	// TODO
+    } 
 
 	MPI_Finalize();
 	return 0;
